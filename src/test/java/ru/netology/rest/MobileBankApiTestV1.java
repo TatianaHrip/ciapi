078874d5a63be3310ccdaf4beac244d6ab8ef4f6
Tatyana Hripkova <tkhripkova@bk.ru>
2021-02-05 22:56:35 +0200

    artifacts  commit

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -2,6 +2,7 @@ package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
+
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
@@ -11,5 +12,6 @@ class MobileBankApiTestV1 {
                 .get("/demo/accounts")
                 .then()
                 .statusCode(200);
+
     }
 }
